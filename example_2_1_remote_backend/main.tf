dd31b87248017b8743656cd39e597d2f50cbe2a5
mmikolajczak <mkm0796@gmail.com>
2021-04-14 01:07:04 +0200

    Example 2.1. – remote backend.

---
+++ example_2_1_remote_backend/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  required_version = ">=0.13.5"
+  # Note: as for the version, the one that is specified is simply the one that was used during development – but
+  # setting this to some lower version should probably be fine, as long as it is equal or above 12.X (which introduced
+  # quite a lot breaking changes)
+
+  # Note: variables can't be used here.
+  backend "s3" {
+    region = "us-east-2"
+    bucket = "putnet-example-terraform-remote-state"
+    key = "putnet-example-terraform-remote-state/terraform.tfstate"
+    dynamodb_table = "putnet-example-terraform-remote-state"
+    encrypt = true
+  }
+}
+
+resource "aws_instance" "example_vm" {
+  ami = "ami-09135e71dc2619458"  # Ubuntu 18.04
+  instance_type = "t2.medium"
+}
